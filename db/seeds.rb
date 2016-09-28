0e56415745088f3eaa7a4f23a48427fd6ed0918e
Frank Vinci <fv42wid@yahoo.com>
2016-09-28 19:45:43 +0000

    create airport model, 2 next

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+#airports
+airports = Airport.create([{code: 'SFO'}, {code: 'NYC'}, {code: 'PHL'}])
