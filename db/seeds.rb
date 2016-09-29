4f415ba356db4346f86a943afa664f167722b634
Frank Vinci <fv42wid@yahoo.com>
2016-09-29 23:33:44 +0000

    seed flights, now 5

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,6 @@
 
 #airports
 airports = Airport.create([{code: 'SFO'}, {code: 'NYC'}, {code: 'PHL'}])
+
+#flights
+trip = Trip.create([{start_airport_id: 1, finish_airport_id: 2, start_time: 1.day.from_now.to_datetime, duration: 90}, {start_airport_id: 2, finish_airport_id: 1, start_time: 2.days.from_now.to_datetime, duration: 100}])
