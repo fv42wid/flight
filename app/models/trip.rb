400e44042c920e7f7d6af44fecf1fa41b26983d1
Frank Vinci <fv42wid@yahoo.com>
2016-09-29 23:27:52 +0000

    add associations, now 4

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,2 +1,4 @@
 class Trip < ApplicationRecord
+  belongs_to :from_airport, class_name: "Airport", foreign_key: "start_airport_id"
+  belongs_to :to_airport, class_name: "Airport", foreign_key: "finish_airport_id"
 end
