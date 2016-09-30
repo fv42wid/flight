81bf1e088b18bf71e257752c442fae9d9190d052
Frank Vinci <fv42wid@yahoo.com>
2016-09-30 23:45:08 +0000

    add search selects to index, now screen 2

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,6 +1,9 @@
 class TripsController < ApplicationController
 
   def index
-
+    @start_airports = Airport.find(Trip.uniq.pluck(:start_airport_id)).map{ |s| [s.code, s.id] }
+    @finish_airports = Airport.find(Trip.uniq.pluck(:finish_airport_id)).map{ |f| [f.code, f.id] }
+    @passengers = [1,2,3,4]
+    @dates = Trip.select("start_time").map{ |time| time.start_time.strftime("%Y%m%d") }.uniq
   end
 end
