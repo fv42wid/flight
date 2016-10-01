9c855f40e919bf6e8dd40f93270d76296876f227
Frank Vinci <fv42wid@yahoo.com>
2016-10-01 19:19:55 +0000

    search works, #4 now select flight form

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,9 +1,20 @@
 class TripsController < ApplicationController
 
   def index
+    @trips = Trip.browse(trip_params[:start_airport_id], trip_params[:finish_airport_id], trip_params[:start_time])
+
     @start_airports = Airport.find(Trip.uniq.pluck(:start_airport_id)).map{ |s| [s.code, s.id] }
     @finish_airports = Airport.find(Trip.uniq.pluck(:finish_airport_id)).map{ |f| [f.code, f.id] }
     @passengers = [1,2,3,4]
     @dates = Trip.select("start_time").map{ |time| time.start_time.strftime("%Y%m%d") }.uniq
+    
+    params[:trip] ||= {:start_airport_id => 0, :finish_airport_id => 0, :passengers => 1, :start_time => 0}
+    
   end
+
+  private
+    def trip_params
+      #use params.fetch here to make trip optional, if user visits index without entering search params
+      params.fetch(:trip, {}).permit(:start_airport_id, :finish_airport_id, :passengers, :start_time)
+    end
 end
--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,4 +1,22 @@
 class Trip < ApplicationRecord
   belongs_to :from_airport, class_name: "Airport", foreign_key: "start_airport_id"
   belongs_to :to_airport, class_name: "Airport", foreign_key: "finish_airport_id"
+
+  def Trip.browse(start_airport, finish_airport, start_time)
+    query = Trip.all
+    query = query.where(start_airport_id: start_airport) if start_airport.present?
+    query = query.where(finish_airport_id: finish_airport) if finish_airport.present?
+    if start_time.present?
+      start_time = start_time.to_datetime
+      end_time = start_time + 1
+      query = query.where(start_time: start_time..end_time)
+    end
+
+    #return value
+    if start_airport.nil? and finish_airport.nil? and start_time.nil?
+      query = Trip.last(10)
+    else
+      query
+    end
+  end
 end
