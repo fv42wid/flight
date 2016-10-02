17d915b2645eb4949d2c084cb9615fd180526a8a
Frank Vinci <fv42wid@yahoo.com>
2016-10-02 16:39:42 +0000

    create booking form, passenger info next, start with 2

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -7,6 +7,7 @@ class TripsController < ApplicationController
     @finish_airports = Airport.find(Trip.uniq.pluck(:finish_airport_id)).map{ |f| [f.code, f.id] }
     @passengers = [1,2,3,4]
     @dates = Trip.select("start_time").map{ |time| time.start_time.strftime("%Y%m%d") }.uniq
+    @booking = Booking.new
     
     params[:trip] ||= {:start_airport_id => 0, :finish_airport_id => 0, :passengers => 1, :start_time => 0}
     
